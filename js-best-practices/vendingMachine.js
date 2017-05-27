73ad6360a240e3ee19f16c32bb30646df33977cc
Peng <sara.thanit@gmail.com>
2017-05-27 16:35:52 -0600

    committed changes done so far
    finished all except the last two exercise in the js-best-practice
    module.

---
+++ js-best-practices/vendingMachine.js
@@ -0,0 +1,40 @@
+
+var balanceManager = require('./balanceManager');
+var changeHandler = require('./changeHandler');
+var productInventory = require('./productInventory');
+
+var products = [
+  {
+    name: 'Skittles',
+    price: 85,
+    id: 'A1'
+  }
+];
+
+module.exports = {
+
+  insertCoin: function(coinType){
+    var value = this.getAmount(coinType);
+    balanceManager.increaseBalance(value);
+  },
+
+  releaseChange: function(){
+    var currentBalance = balanceManager.getBalance();
+    balanceManager.decreaseBalance(currentBalance);
+    return this.convertToChange(currentBalance);
+  },
+  isValidAmount: function (amount) {
+    if (amount === null) {
+      return false
+    } else {
+      return true
+    }
+  },
+
+  vendProduct: function (productId) {
+    var product = productInventory.getProduct(productId);
+    balanceManager.decreaseBalance(product.price);
+    return product;
+  }
+
+};
